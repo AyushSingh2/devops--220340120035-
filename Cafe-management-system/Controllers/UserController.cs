e02ea71362c0fe0b1134142a8b46adce1818fa1d
Ayush Singh <ayushsg.singh@gmail.com>
2023-04-19 22:11:42 +0530

    Cafe management system

---
+++ Cafe-management-system/Controllers/UserController.cs
@@ -0,0 +1,212 @@
+﻿using Cafe_management_system.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.IO;
+using System.Net.Mail;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Cafe_management_system.Controllers
+{
+    [RoutePrefix("api/user")]
+    public class UserController : ApiController
+    {
+        CafeEntities db = new CafeEntities();
+        Response response = new Response();
+
+        [HttpPost, Route("signup")]
+        public HttpResponseMessage Signup([FromBody] User user)
+        {
+            try
+            {
+                User userObj = db.Users
+                    .Where(u => u.email == user.email).FirstOrDefault();
+                if (userObj == null)
+                {
+                    user.role = "user";
+                    user.status = "false";
+                    db.Users.Add(user);
+                    db.SaveChanges();
+                    return Request.CreateResponse(HttpStatusCode.OK, new { message = "Successfully Registered" });
+                }
+                else
+                {
+                    return Request.CreateResponse(HttpStatusCode.BadRequest, new { message = "Email Already Exists" });
+                }
+            }
+            catch (Exception e)
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, e);
+            }
+        }
+
+        [HttpPost, Route("login")]
+        public HttpResponseMessage Login([FromBody] User user)
+        {
+            try
+            {
+                User userObj = db.Users
+                    .Where(u => (u.email == user.email && u.password == user.password)).FirstOrDefault();
+                if (userObj != null)
+                {
+                    if (userObj.status == "true")
+                    {
+                        return Request.CreateResponse(HttpStatusCode.OK, new { token = TokenManager.GenerateToken(userObj.email, userObj.role) });
+                    }
+                    else
+                    {
+                        return Request.CreateResponse(HttpStatusCode.Unauthorized, new { message = "Wait for Admin Approval" });
+                    }
+                }
+                else
+                {
+                    return Request.CreateResponse(HttpStatusCode.Unauthorized, new { message = "Incorrect Username or Password" });
+                }
+            }
+            catch (Exception e)
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, e);
+            }
+        }
+
+        [HttpGet, Route("checkToken")]
+        [CafeAuthenticationFilter]
+
+        public HttpResponseMessage checkToken()
+        {
+            return Request.CreateResponse(HttpStatusCode.OK, new { message = "true" });
+        }
+
+        [HttpGet, Route("getAllUser")]
+        [CafeAuthenticationFilter]
+        public HttpResponseMessage GetAllUser()
+        {
+            try
+            {
+                var token = Request.Headers.GetValues("authorization").First();
+                TokenClaim tokenClaim = TokenManager.ValidateToken(token);
+                if (tokenClaim.Role != "admin")
+                {
+                    return Request.CreateResponse(HttpStatusCode.Unauthorized);
+                }
+                var result = db.Users
+                    .Select(u => new { u.id, u.name, u.contactNumber, u.email, u.status, u.role })
+                    .Where(x => (x.role == "user"))
+                    .ToList();
+                return Request.CreateResponse(HttpStatusCode.OK, result);
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, ex);
+            }
+        }
+
+        [HttpPost, Route("updateUserStatus")]
+        [CafeAuthenticationFilter]
+        public HttpResponseMessage UpdateUserStatus(User user)
+        {
+            try
+            {
+                var token = Request.Headers.GetValues("authorization").First();
+                TokenClaim tokenClaim = TokenManager.ValidateToken(token);
+                if (tokenClaim.Role != "admin")
+                {
+                    return Request.CreateResponse(HttpStatusCode.Unauthorized);
+                }
+                User userObj = db.Users.Find(user.id);
+                if (userObj == null)
+                {
+                    response.message = "User id does not exist";
+                    return Request.CreateResponse(HttpStatusCode.OK, response);
+                }
+                userObj.status = user.status;
+                db.Entry(userObj).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+                response.message = "User Status Updated Successfully";
+                return Request.CreateResponse(HttpStatusCode.OK, response);
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, ex);
+            }
+        }
+
+        [HttpPost, Route("changePassword")]
+        [CafeAuthenticationFilter]
+        public HttpResponseMessage ChangePassword(ChangePassword changePassword)
+        {
+            try
+            {
+                var token = Request.Headers.GetValues("authorization").First();
+                TokenClaim tokenClaim = TokenManager.ValidateToken(token);
+
+                User userObj = db.Users
+                    .Where(x => (x.email == tokenClaim.Email && x.password == changePassword.OldPassword)).FirstOrDefault();
+                if(userObj != null)
+                {
+                    userObj.password = changePassword.NewPassword;
+                    db.Entry(userObj).State = System.Data.Entity.EntityState.Modified;
+                    db.SaveChanges();
+                    response.message = "Password Updated Successfully";
+                    return Request.CreateResponse(HttpStatusCode.OK, response);
+                }
+                else
+                {
+                    response.message = "Incorrect Old Password";
+                    return Request.CreateResponse(HttpStatusCode.BadRequest, response);
+                }
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, ex);
+            }
+        }
+
+        private string createEmailBody(string email, string password)
+        {
+            try
+            {
+                string body = string.Empty;
+                using (StreamReader reader = new StreamReader(HttpContext.Current.Server.MapPath("/Template/forgot-password.html")))
+                {
+                    body = reader.ReadToEnd();
+                }
+                body = body.Replace("{email}", email);
+                body = body.Replace("{password}", password);
+                body = body.Replace("{frontendUrl}", "http://localhost:52990/");
+                return body;
+            } catch(Exception ex)
+            {
+                Console.WriteLine(ex);
+                return null;
+            }
+        }
+
+        [HttpPost,Route("forgotPassword")]
+        public async Task<HttpResponseMessage> ForgotPassword([FromBody] User user)
+        {
+            User userObj = db.Users
+                .Where(x => x.email == user.email).FirstOrDefault();
+            response.message = "Password sent successfully to your email.";
+            if(userObj == null)
+            {
+                return Request.CreateResponse(HttpStatusCode.OK, response);
+            }
+            var message = new MailMessage();
+            message.To.Add(new MailAddress(user.email));
+            message.Subject = "Password by Cafe Management System";
+            message.Body = createEmailBody(user.email, userObj.password);
+            message.IsBodyHtml = true;
+            using(var smtp = new SmtpClient())
+            {
+                await smtp.SendMailAsync(message);
+                await Task.FromResult(0);
+            }
+            return Request.CreateResponse(HttpStatusCode.OK, response);
+        }
+    }
+}
